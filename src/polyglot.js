42d63727dc463b2c36e9b320518e0e8f10c99d93
a.mostovenko <a.mostovenko@smartweb.com.ua>
2016-11-16 18:52:20 +0200

    no need for -o option

--- src/polyglot.js
+++ src/polyglot.js
@@ -4,7 +4,6 @@ import { extractFromFiles } from './extract';
 program
     .command('extract <filepaths ...>')
     .description('Extracts gettext messages and saves them to pot file')
-    .option('-o, --output <path>', 'path where to store pot files')
     .option('-c, --config <path>', 'path to alternative config')
     .option('-i, --interactive', 'Pass all results to stdout')
     .action(extractFromFiles);
