05e2971c418f23d00b455baa896f9a5a01dbea44
a.mostovenko <a.mostovenko@smartweb.com.ua>
2016-12-02 13:08:41 +0200

    added tests for resolve fn-gettext function

---
+++ tests/functional/test_resolve_fn_gettext.js
@@ -0,0 +1,46 @@
+import { expect } from 'chai';
+import * as babel from 'babel-core';
+import polyglotPlugin from 'src/plugin';
+import { rmDirSync } from 'src/utils';
+
+const pofile = 'tests/fixtures/resolve_simple_gettext.po';
+
+const options = {
+    presets: ['es2015'],
+    plugins: [[polyglotPlugin, {
+        resolve: { locale: 'en-us' },
+        locales: {
+            'en-us': pofile,
+        },
+    }]],
+};
+
+describe('Resolve fn-gettext', () => {
+    before(() => {
+        rmDirSync('debug');
+    });
+
+    it('should resolve simple gettext literal (without formatting)', () => {
+        const input = 'console.log(gettext("simple string literal"));';
+        const result = babel.transform(input, options).code;
+        expect(result).to.contain('console.log("simple string literal translated");');
+    });
+
+    it('should resolve original string if no translation is found', () => {
+        const input = 'console.log(gettext("simple string literal without translation"));';
+        const result = babel.transform(input, options).code;
+        expect(result).to.contain('console.log("simple string literal without translation");');
+    });
+
+    it('should resolve original string if no translator notes', () => {
+        const input = 'console.log(gettext("no translator notes"));';
+        const result = babel.transform(input, options).code;
+        expect(result).to.contain('console.log("no translator notes");');
+    });
+
+    it('should throw if has invalid expressions', () => {
+        const input = 'console.log(gettext(fn()));';
+        const func = () => babel.transform(input, options).code;
+        expect(func).to.throw('You can not use CallExpression \'fn()\' as an argument to gettext');
+    });
+});
