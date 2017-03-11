a34cd0b0d7bba1865caba5ab0ce5225b5300fc80
MrOrz <johnsonliang7@gmail.com>
2017-03-11 15:16:09 +0200

    Updated extractors/jsxtag-gettext and unit tests to latest format

--- src/extractors/jsxtag-gettext.js
+++ src/extractors/jsxtag-gettext.js
@@ -1,9 +1,9 @@
 import * as t from 'babel-types';
+import tpl from 'babel-template';
 import gettext from './tag-gettext';
+import { msgid2Orig, hasExpressions,
+    ast2Str, getQuasiStr, dedentStr } from '../utils';
 import { PO_PRIMITIVES } from '../defaults';
-import { template2Msgid, msgid2Orig, hasExpressions,
-    ast2Str, getQuasiStr, strToQuasi, dedentStr } from '../utils';
-import { NoTranslationError } from '../errors';
 
 const { MSGSTR } = PO_PRIMITIVES;
 const NAME = 'jsxtag-gettext';
@@ -12,46 +12,28 @@ function match(node, context) {
     return t.isTaggedTemplateExpression(node) && node.tag.name === context.getAliasFor(NAME);
 }
 
-function resolveDefault(nodePath, poData, context) {
-    const { node } = nodePath;
+function resolveDefault(node, context) {
     const transStr = context.isDedent() ? dedentStr(getQuasiStr(node)) : getQuasiStr(node);
     if (hasExpressions(node)) {
-        nodePath.replaceWithSourceString(strToQuasi(transStr));
-    } else {
-        nodePath.replaceWith(t.stringLiteral(transStr));
+        return node.quasi;
     }
-    return nodePath;
+    return t.stringLiteral(transStr);
 }
 
-function resolve(path, poData, context) {
-    const { translations } = poData;
-    const { node } = path;
-    const msgid = context.isDedent() ? dedentStr(template2Msgid(node)) : template2Msgid(node);
-    const translationObj = translations[msgid];
-
-    if (!translationObj) {
-        throw new NoTranslationError(`No "${msgid}" in "${context.getPoFilePath()}" file`);
-    }
-
-    const transStr = translationObj[MSGSTR][0];
-
-    if (!transStr.length) {
-        throw new NoTranslationError(`No translation for "${msgid}" in "${context.getPoFilePath()}" file`);
-    }
+function resolve(node, translation) {
+    const transStr = translation[MSGSTR][0];
 
     if (hasExpressions(node)) {
         const exprs = node.quasi.expressions.map(ast2Str);
-        path.replaceWithSourceString(msgid2Orig(transStr, exprs));
-    } else {
-        path.replaceWith(t.stringLiteral(transStr));
+        return tpl(msgid2Orig(transStr, exprs))();
     }
+    return t.stringLiteral(transStr);
 }
-
 
 export default {
     ...gettext,
-    resolveDefault,
     resolve,
+    resolveDefault,
     match,
     name: NAME,
 };
--- tests/unit/test_jsxtag_gettext_extractor.js
+++ tests/unit/test_jsxtag_gettext_extractor.js
@@ -1,20 +1,9 @@
 import { expect } from 'chai';
 import gettext from 'src/extractors/jsxtag-gettext';
 import template from 'babel-template';
-import { PO_PRIMITIVES } from 'src/defaults';
 import Context from 'src/context';
 
-const { MSGID } = PO_PRIMITIVES;
-
 const enConfig = new Context();
-
-describe('jsxtag-gettext extract', () => {
-    it('should extract proper msgid ', () => {
-        const node = template('jt`${n} banana`')().expression;
-        const result = gettext.extract({ node }, enConfig);
-        expect(result[MSGID]).to.eql('${ 0 } banana');
-    });
-});
 
 describe('jsxtag-gettext match', () => {
     it('should match gettext', () => {
