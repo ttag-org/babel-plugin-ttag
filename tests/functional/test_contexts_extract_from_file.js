ecae8f157eb08475f75ba913a9de64117c4ec2b3
a.mostovenko <a.mostovenko@smartweb.com.ua>
2017-12-05 13:33:08 +0200

    fix context issue, import + context with filename

---
+++ tests/functional/test_contexts_extract_from_file.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import * as babel from 'babel-core';
+import fs from 'fs';
+import path from 'path';
+import c3po from 'src/plugin';
+import { rmDirSync } from 'src/utils';
+
+
+describe('Contexts extract', () => {
+    before(() => {
+        rmDirSync('debug');
+    });
+
+    it('should extract context from file with filename', () => {
+        const output = 'debug/translations.pot';
+        const options = {
+            plugins: [[c3po, { extract: { output } }]],
+        };
+        const inputFile = 'tests/fixtures/test_context.js';
+        babel.transformFileSync(path.join(process.cwd(), inputFile), options);
+        const result = fs.readFileSync(output).toString();
+        expect(result).to.contain(
+            '#: tests/fixtures/test_context.js:2\nmsgctxt "test ctx"\nmsgid "test"\nmsgstr ""');
+    });
+});
