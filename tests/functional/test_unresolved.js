022d4b25c4192fab01101c3695f6f63066430489
Alexander <a.mostovenko@smartweb.com.ua>
2019-04-26 11:29:08 +0300

    fix issue when resolve is set to 'default' and unresolved is 'fail' (#128)

--- tests/functional/test_unresolved.js
+++ tests/functional/test_unresolved.js
@@ -1,15 +1,22 @@
 import { expect } from 'chai';
 import * as babel from '@babel/core';
-import c3poPlugin from 'src/plugin';
+import ttagPlugin from 'src/plugin';
 import { rmDirSync } from 'src/utils';
 
 const translations = 'tests/fixtures/resolve_simple_gettext.po';
 
 const options = {
     presets: ['@babel/preset-env'],
-    plugins: [[c3poPlugin, {
+    plugins: [[ttagPlugin, {
         resolve: { translations, unresolved: 'fail' },
         discover: ['t', 'ngettext'],
+    }]],
+};
+
+const optionsResolveDefault = {
+    plugins: [[ttagPlugin, {
+        resolve: { translations: 'default', unresolved: 'fail' },
+        discover: ['t'],
     }]],
 };
 
@@ -27,6 +34,11 @@ describe('Unresolved', () => {
         const input = 'console.log(ngettext(msgid`random string`, `random string`, n));';
         const fun = () => babel.transform(input, options).code;
         expect(fun).to.throw('No "random string" in "tests/fixtures/resolve_simple_gettext.po" file');
+    });
+    it('should not fail if resolve: "default"', () => {
+        const input = 'console.log(t`some random string`);';
+        const code = babel.transform(input, optionsResolveDefault).code;
+        expect(code).to.equal('console.log("some random string");');
     });
 });
 
