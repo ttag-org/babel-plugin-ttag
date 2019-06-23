e8a8e911203600a3f7d43f30d3bb8b42cdae959e
Alexander Mostovenko <alexmost1989@gmail.com>
2019-06-23 13:24:58 +0300

    fix lint

--- tests/functional/test_po_resolve.js
+++ tests/functional/test_po_resolve.js
@@ -5,20 +5,19 @@ import { rmDirSync } from 'src/utils';
 
 const translations = 'tests/fixtures/ua.po';
 
-const options = {
-    presets: ['@babel/preset-env'],
-    plugins: [[ttagPlugin, {
-        resolve: { translations },
-        discover: ['ngettext'],
-    }]],
-};
-
 describe('Test po resolve', () => {
     before(() => {
         rmDirSync('debug');
     });
 
     it('should resolve proper plural form of n', () => {
+        const options = {
+            presets: ['@babel/preset-env'],
+            plugins: [[ttagPlugin, {
+                resolve: { translations },
+                discover: ['ngettext'],
+            }]],
+        };
         const expected = 'n % 10 == 1 && n % 100 != 11 ? 0 : ' +
             'n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2';
         const input = 'const n = 1; ' +
@@ -35,7 +34,7 @@ describe('Test po resolve', () => {
         const input = `
         import { t } from "ttag"
         t\`test\`
-        `
+        `;
         const result = babel.transform(input, options).code;
         expect(result).not.to.contain('import');
         expect(result).to.contain('test [translated]');
@@ -49,7 +48,7 @@ describe('Test po resolve', () => {
         const input = `
         const { t } = require("ttag");
         t\`test\`
-        `
+        `;
         const result = babel.transform(input, options).code;
         expect(result).not.to.contain('require');
         expect(result).to.contain('test [translated]');
