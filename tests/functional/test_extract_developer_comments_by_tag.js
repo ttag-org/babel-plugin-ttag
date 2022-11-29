0906a8b950cdcc5caa9e595f6820147f16c3c05b
dhoko <dhoko@users.noreply.github.com>
2022-11-29 13:29:06 +0100

    Support comments before strings inside JSX (#162)
    Co-authored-by: dhoko <agarroux@proton.ch>

--- tests/functional/test_extract_developer_comments_by_tag.js
+++ tests/functional/test_extract_developer_comments_by_tag.js
@@ -7,6 +7,7 @@ import dedent from 'dedent';
 
 const output = 'debug/translations.pot';
 const options = {
+    presets: ['@babel/preset-env', "@babel/preset-react"],
     plugins: [[ttag, { extract: { output }, addComments: 'translator:' }]],
 };
 
@@ -49,5 +50,46 @@ describe('Extract developer comments by tag', () => {
         babel.transform(input, options);
         const result = fs.readFileSync(output).toString();
         expect(result).to.contain('#. test-comment');
+    });
+    it('should extract comments inside JSX tags', () => {
+        const input = dedent(`
+              import { c } from 'ttag';
+
+
+              function render() {
+                return (<>
+                    <h1>{
+                      // translator: this comment is for you AND IT WILL WORK
+                      c('valid-comment-1').t\`Title de ouf\`
+                    }</h1>
+
+                    <h2>{
+                      // this comment is NOT for you 
+                      c('invalid-comment').t\`Title de ouf pas ok?\`
+                    }</h2>
+                    <h2>{
+                      /* translator: OKAY this comment is for you */
+                      c('valid-comment-2').t\`Title de ouf ok?\`
+                    }</h2>
+
+                  <h3>{c('no-comment').t\`hahaha\`}</h3>
+                </>)
+
+              }
+          `);
+        babel.transform(input, options);
+        const result = fs.readFileSync(output).toString();
+        expect(result).to.contain('#. OKAY this comment is for you');
+        expect(result).to.contain('#. this comment is for you AND IT WILL WORK');
+        expect(result).not.to.contain('#. this comment is NOT for you');
+
+        const entries = result.split('\n\n')
+
+        const validComment1 = entries.find((text) => text.includes('valid-comment-1'))
+        const noComment = entries.find((text) => text.includes('no-comment'))
+        const invalidComment = entries.find((text) => text.includes('invalid-comment'))
+        expect(noComment).not.to.contain('#.')
+        expect(invalidComment).not.to.contain('#.')
+        expect(validComment1).to.contain('#. this comment is for you AND IT WILL WORK');
     });
 });
